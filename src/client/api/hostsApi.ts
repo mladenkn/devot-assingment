3e6d8c06cb658d67657544588b1470d6e9e8af5f
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-04 16:57:52 +0200

    installed missing packages, fixed problems

--- src/client/api/hostsApi.ts
+++ src/client/api/hostsApi.ts
@@ -1,15 +1,9 @@
-import { GetHostsRequest, serializeGetHostsRequest } from "../../../shared";
+import { GetHostsRequest, serializeGetHostsRequest } from "../../shared";
 import axios from 'axios'
-import { format } from 'date-fns'
 
 export function useHostsApi(){
 
   function get(req: GetHostsRequest){
-    const params = {
-      startDate: format(req.startDate, 'yyyy-MM-dd'),
-      endDate: format(req.startDate, 'yyyy-MM-dd'),
-      guests: req.guests
-    }
     return axios.get('http://localhost:3001/hosts/search/', { params: serializeGetHostsRequest(req) })
   }
 
