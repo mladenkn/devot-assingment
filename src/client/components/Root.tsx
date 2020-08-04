bb2d1171fa53c5bc3d8ff41de51516ee91abd384
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-04 17:37:58 +0200

    updated frontedn

--- src/client/components/Root.tsx
+++ src/client/components/Root.tsx
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHostsApi } from '../api/hostsApi';
 import { SearchHostsForm } from './SearchHostsForm';
 import { makeStyles } from '@material-ui/core';
+import { SearchHostsRequest } from '../../shared';
+
+interface State {
+  form: SearchHostsRequest
+  response: string
+}
+
+const initialSearchFormData = {
+  startDate: new Date(),
+  endDate: new Date(),
+  guests: 5
+}
 
 export function Root() {
 
   const hostsApi = useHostsApi()
-  const initialSearchFormData = {
-    startDate: new Date(),
-    endDate: new Date(),
-    guests: 5
+
+  const [state, updateState] = useState<State>({
+    form: initialSearchFormData,
+    response: ''
+  })
+
+  function loadHosts(form: SearchHostsRequest){
+    hostsApi.search(form)
+      .then(r => {
+        updateState(oldState => ({
+          form: oldState.form,
+          response: JSON.stringify(r.data, null, 2)
+        }))
+      })
   }
 
-  hostsApi.search(initialSearchFormData)
-    .then(console.log)
-
+  useEffect(() => loadHosts(state.form), [])
+  
   const styles = useStyles()
 
   return (
     <div>
-      <SearchHostsForm className={styles.form} value={initialSearchFormData} />    
+      <SearchHostsForm className={styles.form} value={state.form} />
+      {state.response} 
     </div>
   );
 }
