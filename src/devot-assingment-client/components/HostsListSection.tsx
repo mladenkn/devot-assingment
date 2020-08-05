998c999d943c8bc8a40c6c37b376ae86c789084b
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-06 01:50:14 +0200

    updated Loadable

--- src/devot-assingment-client/components/HostsListSection.tsx
+++ src/devot-assingment-client/components/HostsListSection.tsx
@@ -4,6 +4,7 @@ import { makeStyles } from '@material-ui/core';
 import { useHomeSectionLogic } from '../logic/homeSectionLogic';
 import { SearchHostsRequest, SearchHostsRequestUncomplete } from '../../devot-assingment-shared/models';
 import { HostsList } from './HostList';
+import { Loadable } from '../../utils/view/Loadable';
 
 type Props = {
   formInitialValues: SearchHostsRequest | SearchHostsRequestUncomplete
@@ -16,7 +17,14 @@ export function HostsListSection(props: Props) {
   return (
     <div className={styles.root}>
       <SearchHostsForm form={logic.form} />
-      {logic.response.status === 'LOADED' && <HostsList className={styles.hostsList} hosts={logic.response.value} />}
+      <Loadable
+        {...logic.response}
+        className={styles.hostsListWrapper} 
+        circularProgressStyle={{ width: 70, height: 70 }} 
+        notLoadedClassName={styles.hostsListNotLoaded}
+      >
+        {hosts => <HostsList hosts={hosts} />}
+      </Loadable>
     </div>
   );
 }
@@ -25,7 +33,14 @@ const useStyles = makeStyles({
   root: {
     padding: '45px 700px 0px 50px',
   },
-  hostsList: {
+  hostsListWrapper: {
     marginTop: 10
+  },
+  hostsListNotLoaded: {
+    width: 600,
+    height: 400
+  },
+  circularProgress: {
+    width: 60
   }
 })
---
+++ src/utils/view/Loadable.tsx
@@ -0,0 +1,31 @@
+import { Loadable as LoadableModel } from "../loadable";
+import { makeStyles, CircularProgress } from "@material-ui/core";
+import React, { ReactElement } from "react";
+import clsx from 'clsx'
+import { CSSProperties } from "@material-ui/core/styles/withStyles";
+
+type Props<T> = {
+  className?: string
+  notLoadedClassName?: string
+  circularProgressStyle?: CSSProperties
+  children: (loaded: T) => ReactElement
+} & LoadableModel<T>
+
+export function Loadable<TValue>(props: Props<TValue>){
+  const styles = useStyles()
+  return (
+    <div className={clsx(props.className, props.status !== 'LOADED' && styles.notLoaded, props.status !== 'LOADED' && props.notLoadedClassName)}>
+      {props.status === 'ERROR' && 'Error'}
+      {props.status === 'LOADING' && <CircularProgress style={props.circularProgressStyle} />}
+      {props.status === 'LOADED' && props.children(props.value)}
+    </div>
+  )
+}
+
+const useStyles = makeStyles({
+  notLoaded: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
