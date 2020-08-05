fc94a12a4045a418feecd24af827dde5a40bfd74
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-05 23:13:50 +0200

    updated

--- src/devot-assingment-server/routes.ts
+++ src/devot-assingment-server/routes.ts
@@ -1,11 +1,14 @@
 import { IRouter } from "express";
 import { HostsRepository } from "./HostsRepository";
-import { deserialize } from "../devot-assingment-shared/SearchHostsRequest";
 
 export function registerRoutes(router: IRouter, hostsRepo: HostsRepository){
 
   router.get('/hosts/search', async (req, res) => {
-    const params = deserialize(req.query)
+    const params = {
+      startDate: new Date(req.query.startDate as string),
+      endDate: new Date(req.query.endDate as string),
+      guestsCount: parseInt(req.query.guestsCount as string)
+    }
     const hosts = await hostsRepo.search(params)
     res.json(hosts)
   })
--- src/devot-assingment-shared/SearchHostsRequest.ts
+++ src/devot-assingment-shared/SearchHostsRequest.ts
@@ -9,11 +9,3 @@ export type SearchHostsRequestUncomplete = {
   endDate: Date | null,
   guestsCount?: number
 }
-
-export function deserialize(serialized: any): SearchHostsRequest{
-  return {
-    startDate: new Date(serialized.startDate),
-    endDate: new Date(serialized.endDate),
-    guestsCount: parseInt(serialized.guestsCount)
-  }
-}
