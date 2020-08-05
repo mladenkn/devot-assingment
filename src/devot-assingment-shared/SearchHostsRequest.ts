f1437f101d6cff8d8a6d7ec3c12850c6bf7c5a00
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-05 14:19:10 +0200

    .

--- src/devot-assingment-shared/SearchHostsRequest.ts
+++ src/devot-assingment-shared/SearchHostsRequest.ts
@@ -10,14 +10,6 @@ export type SearchHostsRequestUncomplete = {
   guests?: number
 }
 
-// export function serialize(r: SearchHostsRequest){
-//   return {
-//     startDate: format(r.startDate, 'yyyy-MM-dd'),
-//     endDate: format(r.startDate, 'yyyy-MM-dd'),
-//     guests: r.guests
-//   }
-// }
-
 export function deserialize(serialized: any){
   return {
     startDate: new Date(serialized.startDate),
