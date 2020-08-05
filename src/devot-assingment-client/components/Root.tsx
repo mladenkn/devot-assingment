3de5174d332375ef273832e74374135044edf799
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-06 01:07:05 +0200

    HostsList --> HostsListSection

--- src/devot-assingment-client/components/Root.tsx
+++ src/devot-assingment-client/components/Root.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
-import { HostsList } from "./HostsList";
+import { HostsList } from "./HostsListSection";
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
 export function Root(){
