8480d2a892e3c98e1f13320bfd401e9406e8774a
Mladen Knezović <mladen.knezovic.1993@gmail.com>
2020-08-07 17:22:43 +0200

    done seeding db

---
+++ src/devot-assingment-server-devops/seedDb.ts
@@ -0,0 +1,31 @@
+import path from 'path'
+import loadAppData from '../devot-assingment-server/loadAppData'
+import { connectToDb } from '../devot-assingment-server/dbInit'
+import { Host } from '../devot-assingment-server/models'
+
+function getDataFilesPaths(){
+  // TODO: softcode path!
+  const folder = 'C:\\Users\\mlade\\Documents\\projekti\\devot-assingment\\assingment_specs'
+  const sufix = '-2000.csv'
+  return {
+    bookings: path.join(folder, 'bookings' + sufix),
+    hosts: path.join(folder, 'hosts' + sufix),
+    rooms: path.join(folder, 'rooms' + sufix),
+  }
+}
+
+loadAppData(getDataFilesPaths())
+  .then(async data => {
+    const db = connectToDb()
+    await db.table<Host>('hosts').insert(data.hosts)
+    await db.table<Host>('rooms').insert(data.rooms)
+    await db.table<Host>('bookings').insert(data.bookings)
+  })
+  .then(() => {
+    console.log('Successfully seeded the database\n')
+    process.exit(0)
+  })
+  .catch(e => {
+    console.error(e)
+    process.exit(1)
+  })
